34054cb1deb1e061bb94904dcd3dc545759848a4
Diwaker Ghimire <idwaker@gmail.com>
2014-09-09 09:38:27 +0545

    updated modules with translations

--- Module.php
+++ Module.php
@@ -2,6 +2,8 @@
 
 namespace idwaker\auth;
 
+use Yii;
+
 
 class Module extends \yii\base\Module
 {
@@ -10,7 +12,21 @@ class Module extends \yii\base\Module
     public function init()
     {
         parent::init();
+        $this->registerTranslations();
 
         // custom initialization code goes here
+    }
+    
+    public function registerTranslations()
+    {
+        Yii::$app->i18n->translations['auth'] = [
+           'class' => 'yii\i18n\PhpMessageSource',
+           'sourceLanguage' => '',
+           'basePath' => '@idwaker/auth/messages',
+           
+           // 'fileMap' => [
+//                'auth' => 'auth.php'
+//            ] 
+        ];
     }
 }
