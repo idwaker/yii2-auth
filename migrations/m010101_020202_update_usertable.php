563c3053fa51edaab2dfaf6c6654ba2a180874f2
Diwaker Ghimire <idwaker@gmail.com>
2014-09-15 09:32:53 +0545

    renamed parent to respective table_id and fixed dropIndex missing tablename

--- migrations/m010101_020202_update_usertable.php
+++ migrations/m010101_020202_update_usertable.php
@@ -18,6 +18,8 @@ class m010101_020202_update_usertable extends Migration
                            Schema::TYPE_TEXT . ' NOT NULL');
         $this->createIndex('username_unique', 'auth_user', ['username'],
                             true);
+        $this->renameColumn('auth_role', 'parent', 'role_id');
+        $this->renameColumn('auth_permission', 'parent', 'permission_id');
     }
 
     public function safeDown()
@@ -25,6 +27,8 @@ class m010101_020202_update_usertable extends Migration
         $this->dropColumn('auth_user', 'secret_key');
         $this->alterColumn('auth_user', 'auth_key',
                            Schema::TYPE_STRING . '(32) NOT NULL');
-        $this->dropIndex('username_unique');
+        $this->dropIndex('username_unique', 'auth_user');
+        $this->renameColumn('auth_role', 'role_id', 'parent');
+        $this->renameColumn('auth_permission', 'permission_id', 'parent');
     }
 }
